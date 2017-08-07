658c80519457770cb045f684184d670227144726
olivier-d <olivierdegreve93@gmail.com>
2017-08-07 22:06:43 +0200

    Html : Connexion + Inscription
    Ajout bootstrap + html : div connexion, div inscription

--- views/dist/js/script.js
+++ views/dist/js/script.js
@@ -1,3 +1,31 @@
 $(document).ready(function() {
-    console.log("Hello World !");
+
+	$('#divConnexion').css('display', 'block');
+
+    
+    $('#butNotYetRegister').click(function() {
+        toggleDiv('divInscription');
+    });
+    $('#butRegisterReturn').click(function() {
+        toggleDiv('divConnexion');
+    });
 });
+
+
+function toggleDiv(divToDisplay) {
+    var currentDisplay = getCurrentDisplaySection();
+    $('#' + currentDisplay).attr('style', 'display:none');
+    $('#' + divToDisplay).attr('style', 'display:block');
+    $('#' + divToDisplay + " input:first").focus();
+}
+
+function getCurrentDisplaySection() {
+    var res;
+
+    $("div[id^='div']").each(function() {
+        if ($(this).css('display') == "block") {
+            res = $(this).attr('id');
+        }
+    });
+    return res;
+}

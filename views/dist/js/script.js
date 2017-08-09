e09c0cd1e6f16aeb80346d098fed8eeb2b4c058f
Dylan Di Vito <divitodylan@gmail.com>
2017-08-09 13:32:10 +0200

    Work in Progress on Frontend script.js index.html

--- views/dist/js/script.js
+++ views/dist/js/script.js
@@ -9,6 +9,35 @@ $(document).ready(function() {
     $('#butRegisterReturn').click(function() {
         toggleDiv('divConnexion');
     });
+    $('#createButton').click(function () {
+        toggleDiv('divManagement')
+    })
+
+    $('#displayGames').click(function () {
+
+    })
+
+    $('#displayInvitations').click(function () {
+        toggleDiv('divInvitations')
+    })
+
+    $('#displayManagement').click(function () {
+        toggleDiv('divManagement')
+    })
+
+    $('#displayNight').click(function () {
+
+    })
+
+
+
+    $('#addGameButton').click(function () {
+        $('#addGameButton').before('<div class="input-group" style="margin-bottom: 20px">' +
+            '<select id="jsGameX" class="form-control">\n' +
+            '<option>A IMPLEMENTER EN JS</option>\n' +
+            '</select>' +
+            '</div>')
+    })
 });
 
 
@@ -29,3 +58,13 @@ function getCurrentDisplaySection() {
     });
     return res;
 }
+
+function getCurrentDisplayUnderSection(currentDiv, divToDisplay){
+    $("currentDiv div[id^='section']").each(function() {
+        if ($(this).css('display') == "block") {
+            res = $(this).attr('id');
+        }
+    });
+    return res;
+}
+

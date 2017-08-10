0291e175b5eac5d66a0f0172d417e06424e9da14
olivier-d <olivierdegreve93@gmail.com>
2017-08-10 11:29:00 +0200

    JS : Pseudo-objets - notify - nav load

--- views/dist/js/script.js
+++ views/dist/js/script.js
@@ -1,14 +1,7 @@
 $(document).ready(function() {
 
-	$('#divConnexion').css('display', 'block');
+    Init.all();
 
-    
-    $('#butNotYetRegister').click(function() {
-        toggleDiv('divInscription');
-    });
-    $('#butRegisterReturn').click(function() {
-        toggleDiv('divConnexion');
-    });
     $('#createButton').click(function () {
         toggleSectionManagement(1);
     })
@@ -19,28 +12,7 @@ $(document).ready(function() {
 
     $('#validateNightButton').click(function () {
         toggleSectionManagement(2);
-    })
-
-    $('#displayGames').click(function () {
-        activeNavItem('game');
-        toggleDiv('divGames');
-    })
-
-    $('#displayInvitations').click(function () {
-        activeNavItem('invite');
-        toggleDiv('divInvitations');
-    })
-
-    $('#displayManagement').click(function () {
-        activeNavItem('management');
-        toggleDiv('divManagement');
-    })
-
-    $('#displayNight').click(function () {
-        activeNavItem('night');
-    })
-
-
+    });
 
 
     $('#addUserEmailButton').click(function () {
@@ -59,43 +31,35 @@ $(document).ready(function() {
             '</select>' +
             '</div>')
     })
+
+
+    $('#navJeux').click(function() {
+        $('#section-jeux').attr('style', 'display:block');
+    })
+
+
 });
 
+var User = (function() {
+    function logIn() {
+        Utils.notifySucces("Connexion réussie");
+        Init.navUser();
+    }    
 
-function toggleDiv(divToDisplay) {
-    var currentDisplay = getCurrentDisplaySection();
-    $('#' + currentDisplay).attr('style', 'display:none');
-    $('#' + divToDisplay).attr('style', 'display:block');
-    $('#' + divToDisplay + " input:first").focus();
-}
+    function logOut() {
 
-function activeNavItem(navItemToDisplay) {
-    var currentActive = getCurrentActiveNavItem()
-    $(currentActive).removeClass("active")
-    $('#' + navItemToDisplay).addClass("active");
-}
+    }
 
-function getCurrentActiveNavItem() {
-    var res;
+    function register() {
 
-    $("#navbar li").each(function () {
-        if($(this).hasClass("active")) {
-            res = $(this);
-        }
-    });
-    return res;
-}
+    }
+    return {
+        logIn: logIn,
+        logOut: logOut,
+        register: register
+    }       
+})();
 
-function getCurrentDisplaySection() {
-    var res;
-
-    $("div[id^='div']").each(function() {
-        if ($(this).css('display') == "block") {
-            res = $(this).attr('id');
-        }
-    });
-    return res;
-}
 
 function toggleSectionManagement(type) {
     if(type === 1){
@@ -116,4 +80,148 @@ function toggleSectionManagement(type) {
 
 }
 
+// Visual + Click event
+var Init = (function() {
+    
+    function all() {
+        $('#divConnexion').css('display', 'block');
+        $('#butNotYetRegister').click(function() {
+            Utils.toggleDiv('divInscription');
+        });
+        $('#butRegisterReturn').click(function() {
+            Utils.toggleDiv('divConnexion');
+        });
+        $('#butLogIn').click(function() {
+            User.logIn();
+        });
+        
+        initClickEvents();
+    }
+    
+    function initClickEvents() {
+        initNav();
+        //initButtonForm();
+    }
 
+    // When the user log in
+    function navUser() {
+        $('#divConnexion').attr('style','display:none');
+        $('#divNavBar').attr('style','display:block');
+        $('#divInvitations').attr('style','display:block');
+    }
+
+    function initNav() {
+        $('#navbar li a').each(function(index, element) {
+            if (element.id === "disconnection") {
+                // TODO traitement déconnexion
+            } else {
+                $(element).click(function() {
+                    var idDiv = "div" + element.id.substring(7);
+                    Utils.toggleDiv(idDiv);
+                    Utils.activeNavItem($(element).parent().attr('id'));
+                })
+            }
+        })
+    }
+
+    function initButtonForm() {
+
+    }
+
+    return {
+        all: all,
+        initClickEvents: initClickEvents,
+        navUser: navUser
+    }
+
+})();
+
+
+var Utils = (function() {
+
+    function toggleDiv(divToDisplay) {
+        var currentDisplay = getCurrentDisplaySection();
+        $('#' + currentDisplay).attr('style', 'display:none');
+        $('#' + divToDisplay).attr('style', 'display:block');
+        $('#' + divToDisplay + " input:first").focus();
+    }
+
+    function activeNavItem(navItemToDisplay) {
+        var currentActive = getCurrentActiveNavItem()
+        $(currentActive).removeClass("active")
+        $('#' + navItemToDisplay).addClass("active");
+    }
+
+    function getCurrentActiveNavItem() {
+        var res;
+
+        $("#navbar li").each(function () {
+            if($(this).hasClass("active")) {
+                res = $(this);
+            }
+        });
+        return res;
+    }
+
+    function getCurrentDisplaySection() {
+        var res;
+
+        $("div[id^='div']").each(function() {
+            if ($(this).css('display') == "block") {
+                res = $(this).attr('id');
+            }
+        });
+        return res;
+    }
+
+    function notifySucces(data) {
+        $.notify({
+            icon: 'fa fa-check-circle-o',
+            title: '<b>Succès</b> : ',
+            message: data
+        },{
+            type: 'success',
+            allow_dismiss: true,
+            placement: {
+                from: "bottom",
+                align: "right"
+            },
+            offset: {
+                x: 20,
+                y: 40
+            },
+            spacing: 10,
+            z_index: 1031,
+            delay: 5000,
+        });
+    }
+
+    function notifyError(data) {
+        $.notify({
+            icon: 'fa fa-times-circle-o',
+            title: '<b>ERREUR</b> : ',
+            message: data
+        },{
+            type: 'danger',
+            allow_dismiss: true,
+            placement: {
+                from: "bottom",
+                align: "right"
+            },
+            offset: {
+                x: 20,
+                y: 40
+            },
+            spacing: 10,
+            z_index: 1031,
+            delay: 5000,
+        });
+    }
+
+    return {
+        toggleDiv: toggleDiv,
+        activeNavItem: activeNavItem,
+        notifySucces: notifySucces,
+        notifyError: notifyError
+    }
+})();

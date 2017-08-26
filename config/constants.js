e442d6fa6062d4b6bfbde0a79224b412d89b85c5
Aedelon <delanoe.pirard@gmail.com>
2017-08-26 14:27:34 +0200

    * debug * add constant * routes improvment

--- config/constants.js
+++ config/constants.js
@@ -6,8 +6,9 @@ function define(name, value) {
 }
 
 define("SERVER_ADDRESS", "http://localhost:3000");
-define("CLIENT_ADDRESS", "http://localhost:3000");
+define("CLIENT_ADDRESS", "http://localhost:5000");
 
 define("CREATED_NIGHT", "CREATED");
 define("CONFIRMED_NIGHT", "CONFIRMED");
 define("CANCELLED_NIGHT", "CANCELLED");
+define("FINISHED_NIGHT", "FINISHED");
